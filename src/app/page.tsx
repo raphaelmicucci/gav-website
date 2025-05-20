92cde5912d56e5f9ae2061ecb9b75d3e2ea82f6b
Raphael <raphaelmicucci@gmail.com>
2025-05-20 19:54:18 -0300

    Commit inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            Aqui está seu novo site Next.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
